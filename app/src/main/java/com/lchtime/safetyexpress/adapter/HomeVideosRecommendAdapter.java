3052a86251914678dfd91f764c42e099feeee59b
cp <123456>
2017-05-09 19:07:08 +0800

    首页接口完成，圈子完成一半

--- app/src/main/java/com/lchtime/safetyexpress/adapter/HomeVideosRecommendAdapter.java
+++ app/src/main/java/com/lchtime/safetyexpress/adapter/HomeVideosRecommendAdapter.java
@@ -1,81 +1,103 @@
 package com.lchtime.safetyexpress.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.lchtime.safetyexpress.R;
+import com.lchtime.safetyexpress.bean.NewsBean;
+import com.lchtime.safetyexpress.ui.news.MediaActivity;
+import com.lchtime.safetyexpress.utils.CommonUtils;
+import com.squareup.picasso.Picasso;
 
-import java.util.List;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
- * Created by user on 2017/4/18.
+ * Created by android-cp on 2017/5/8.
  */
 
-public class HomeVideosRecommendAdapter extends BaseAdapter {
-
+public class HomeVideosRecommendAdapter extends RecyclerView.Adapter{
     private Context context;
     private LayoutInflater inflater;
-    private List<String> list;
+    private ArrayList<NewsBean> videoList;
 
-    public HomeVideosRecommendAdapter(Context context) {
+    public HomeVideosRecommendAdapter(Context context, ArrayList<NewsBean> videoList) {
         this.context = context;
+        this.videoList = videoList;
         inflater = LayoutInflater.from(context);
     }
-
     @Override
-    public int getCount() {
-        return list == null ? 10 : list.size();
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.home_videos_recommend_item, null);
+        return new ViewHolder(view);
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        ViewHolder myHolder = (ViewHolder)holder;
+        final NewsBean bean = videoList.get(position);
+
+        myHolder.tv_title.setText(bean.cc_title);
+        Picasso.with(context).load(bean.media.get(0)).fit().into(myHolder.iv_img);
+        myHolder.tv_from.setText(bean.cc_from);
+        myHolder.tv_comment.setText(bean.plNum);
+
+        myHolder.tv_time2.setText( CommonUtils.getSpaceTime(Long.parseLong(bean.cc_datetime)));
+        myHolder.tv_playnum.setText(bean.cc_count + "次播放");
+        myHolder.rl_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MediaActivity.class);
+                intent.putExtra("url",bean.media.get(1));
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public int getItemCount() {
+        return videoList == null ? 0 : videoList.size();
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder = null;
-        if (convertView == null) {
-            convertView = inflater.inflate(R.layout.home_videos_recommend_item, null);
-            holder = new ViewHolder();
-            holder.tv_title = (TextView) convertView.findViewById(R.id.tv_recommend_title);
-            holder.ll_share = (LinearLayout) convertView.findViewById(R.id.ll_recommend_share);
-            holder.iv_img = (ImageView) convertView.findViewById(R.id.iv_recommend_img);
-            holder.iv_img_bg = (ImageView) convertView.findViewById(R.id.iv_recommend_img_bg);
-            holder.iv_play = (ImageView) convertView.findViewById(R.id.iv_recommend_play);
-            holder.tv_time1 = (TextView) convertView.findViewById(R.id.tv_recommend_time1);
-            holder.tv_from = (TextView) convertView.findViewById(R.id.tv_recommend_from);
-            holder.tv_comment = (TextView) convertView.findViewById(R.id.tv_recommend_comment);
-            holder.tv_time2 = (TextView) convertView.findViewById(R.id.tv_recommend_time2);
-            holder.tv_playnum = (TextView) convertView.findViewById(R.id.tv_recommend_playnum);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-        return convertView;
-    }
+    class ViewHolder extends RecyclerView.ViewHolder{
 
-    class ViewHolder {
+        @BindView(R.id.tv_recommend_title)
         TextView tv_title;
+        @BindView(R.id.ll_recommend_share)
         LinearLayout ll_share;
+        @BindView(R.id.iv_recommend_img)
         ImageView iv_img;
+        @BindView(R.id.iv_recommend_img_bg)
         ImageView iv_img_bg;
+        @BindView(R.id.iv_recommend_play)
         ImageView iv_play;
+        @BindView(R.id.tv_recommend_time1)
         TextView tv_time1;
+        @BindView(R.id.tv_recommend_from)
         TextView tv_from;
+        @BindView(R.id.tv_recommend_comment)
         TextView tv_comment;
+        @BindView(R.id.tv_recommend_time2)
         TextView tv_time2;
+        @BindView(R.id.tv_recommend_playnum)
         TextView tv_playnum;
+        @BindView(R.id.rl_play)
+        RelativeLayout rl_play;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
     }
 }
---
+++ app/src/main/java/com/lchtime/safetyexpress/bean/res/VideoRes.java
@@ -0,0 +1,8 @@
+package com.lchtime.safetyexpress.bean.res;
+
+/**
+ * Created by android-cp on 2017/5/8.
+ */
+
+public class VideoRes {
+}
